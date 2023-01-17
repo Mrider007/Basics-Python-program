0e189689bb476d67f1bada92c31571301054c1aa
Mukesh Kumar Singh <122354876+Mrider007@users.noreply.github.com>
2023-01-18 00:14:31 +0530

    Add files via upload

---
+++ oprator.py
@@ -0,0 +1,24 @@
+#oprator
+a= 3
+b= 4
+
+print("The value of 3+4 is", 3+4)
+print("the value of 3-4 is", 3-4)
+print("the value of 3*4 is", 3*4)
+print("the value of 3/4 is", 3/4)
+# Assiment oprators
+a=34
+a *=2
+a /=5
+print(a)
+
+# comparision oprators
+b=(14>=7)
+print (b)
+
+# Logical oprators
+bool1 = True 
+bool2= False 
+print ("the vlue of bool1 and bool2 is",(bool1 and bool2))
+print ("the value of bool1 or bool2 is",(bool1 or bool2))
+print ("the value of not bool2 is",(not bool2))
